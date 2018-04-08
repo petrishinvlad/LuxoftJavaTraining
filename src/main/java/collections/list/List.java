39527bbc599e004ffabf39756180f7a7d63c46d4
Vlad Petrishin <vladpetrishin@Vlads-MacBook-Pro.local>
2018-04-08 13:22:44 +0300

    Adding List interface

---
+++ src/main/java/collections/list/List.java
@@ -0,0 +1,27 @@
+package collections.list;
+
+public interface List {
+    void add(Object value);
+
+    void add(Object value, int index);
+
+    Object remove(int index);
+
+    Object get(int index);
+
+    Object set(Object value, int index);
+
+    void clear();
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean contains(Object value);
+
+    int indexOf(Object value);
+
+    int lastIndexOf(Object value);
+
+    String toString();
+}
